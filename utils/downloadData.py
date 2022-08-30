1300468ec03204846b68d09bed823491f5bf5705
Mingzhou Yang <yang7492@umn.edu>
2022-08-29 21:57:16 -0500

    add requests

--- utils/downloadData.py
+++ utils/downloadData.py
@@ -1,1 +1,16 @@
-from utils.apiKeys import *
+from apiKeys import NRELapi,Censusapi
+import requests
+
+def getRequest(url, query):
+    return requests.get(url, params= query)
+
+stationQuery = {'api_key':NRELapi.getKey(),'fuel_type':'E85,ELEC','state':'CA','limit':'2'}
+stationUrl = 'https://developer.nrel.gov/api/alt-fuel-stations/v1.json'
+stationData = getRequest(stationUrl, stationQuery)
+
+censusUrl = 'https://api.census.gov/data/2019/acs/acs1'
+censusQuery = {'get': 'NAME,B01001_001E', 'key':Censusapi.getKey()}
+censusData = getRequest(censusUrl, censusQuery)
+
+print("test charging station:" + stationData.text)
+print("test census data: "+ censusData.text)
