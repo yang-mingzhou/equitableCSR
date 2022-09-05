ab0f14d9b2106573ca166b62e7b5c118dda17a26
Mingzhou Yang <yang7492@umn.edu>
2022-09-05 17:37:52 -0500

    refactor the plot functions

--- utils/osmGraph.py
+++ utils/osmGraph.py
@@ -18,15 +18,15 @@ class OsmGraph:
     def graphToGdfs(self):
         nodes, edges = ox.graph_to_gdfs(self.graph, nodes=True, edges=True)
         return nodes, edges
+    
+    def plot(self):
+        ox.plot.plot_graph(self.graph)
+        return
 
     def getEdges(self):
         _, edges = self.graphToGdfs()
         return edges
 
-    def getEdgesDict(self):
-        _, edges = self.graphToGdfs()
-        return edges.to_dict('index')
-    
     def getNearestNode(self, x, y):
         return ox.distance.nearest_nodes(self.graph, y, x, return_dist=True)
     
@@ -58,6 +58,42 @@ class OsmGraph:
         return traj
     
     @staticmethod
+    def __initGoFigure(lat, long, type='markers', label='default', size=5, color='red'):
+        '''
+        type = 'markers' or 'lines'
+        lat/long: list, if 'type == lines'; float, else
+        '''
+        fig = go.Figure(go.Scattermapbox(
+                    name=label,
+                    mode=type,
+                    lon=long,
+                    lat=lat,
+                    marker={'size': size, 'color': color}))
+        return fig
+    
+    @staticmethod
+    def __addTrace(fig, lat, long, type='markers', label='default', size=5, color='red'):
+        fig.add_trace(go.Scattermapbox(
+                    name=label,
+                    mode=type,
+                    lon=long,
+                    lat=lat,
+                    marker={'size': size, 'color': color}))
+        return fig
+    
+    @staticmethod
+    def __plotFigAndSave(fig, lat_center, long_center, filename):
+        fig.update_layout(mapbox_style="stamen-terrain",
+                          mapbox_center_lat=30, mapbox_center_lon=-80)
+        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
+                          mapbox={
+                              'center': {'lat': lat_center,
+                                         'lon': long_center},
+                              'zoom': 9.5})
+        plotly.offline.plot(fig, filename=filename, auto_open=False)
+        return
+         
+    @staticmethod
     def plotTrajList(trajList, filename):
         '''
         trajList: [traj]
@@ -67,34 +103,18 @@ class OsmGraph:
             lat, long = zip(*traj)
             # adding the lines joining the nodes
             if i == 0:
-                fig = go.Figure(go.Scattermapbox(
-                    mode="lines",
-                    lon=long,
-                    lat=lat,
-                    marker={'size': 5, 'color': 'red'}))
+                fig = OsmGraph.__initGoFigure(lat, long, type='lines', label='traj'+str(i))
                 
             else:
-                fig.add_trace(go.Scattermapbox(
-                    mode="lines",
-                    lon=long,
-                    lat=lat,
-                    marker={'size': 5, 'color': 'red'}))
+                fig = OsmGraph.__addTrace(fig, lat, long, type='lines', label='traj'+str(i))
             i += 1
         # getting center for plots:
         lat_center = np.mean(lat)
         long_center = np.mean(long)
-        # defining the layout using mapbox_style
-        fig.update_layout(mapbox_style="stamen-terrain",
-                          mapbox_center_lat=30, mapbox_center_lon=-80)
-        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
-                          mapbox={
-                              'center': {'lat': lat_center,
-                                         'lon': long_center},
-                              'zoom': 9.5})
-        plotly.offline.plot(fig, filename=filename, auto_open=False)
+        OsmGraph.__plotFigAndSave(fig, lat_center, long_center, filename)
         
     @staticmethod
-    def plotData(pointDict, colorList,filename):
+    def plotPointList(pointDict, colorList,filename):
         '''
         pointDict: {"label": list of points}
         '''
@@ -103,33 +123,41 @@ class OsmGraph:
             lat, long = zip(*pointDict[label])
             # adding the lines joining the nodes
             if i == 0:
-                fig = go.Figure(go.Scattermapbox(
-                    name = label,
-                    mode="markers",
-                    lon=long,
-                    lat=lat,
-                    marker={'size': 15, 'color': colorList[i]}))
+                fig = OsmGraph.__initGoFigure(lat, long, type='markers', label= label, size=15, color=colorList[i])
                 
             else:
-                fig.add_trace(go.Scattermapbox(
-                    name= label,
-                    mode="markers",
-                    lon=long,
-                    lat=lat,
-                    marker={'size': 15, 'color': colorList[i]},))
+                fig = OsmGraph.__addTrace(fig, lat, long, type='markers', label= label, size=15, color=colorList[i])
             i += 1
         # getting center for plots:
         lat_center = np.mean(lat)
         long_center = np.mean(long)
-        # defining the layout using mapbox_style
-        fig.update_layout(mapbox_style="stamen-terrain",
-                          mapbox_center_lat=30, mapbox_center_lon=-80)
-        fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0},
-                          mapbox={
-                              'center': {'lat': lat_center,
-                                         'lon': long_center},
-                              'zoom': 9.5})
-        plotly.offline.plot(fig, filename=filename, auto_open=False)
+        OsmGraph.__plotFigAndSave(fig, lat_center, long_center, filename)
+        
+    @staticmethod
+    def plotTrajAndPoint(trajList, pointDict, colorList, filename):
+        '''
+        trajList: [traj]
+        pointDict: {"label": list of points}
+        '''
+        i = 0
+        for traj in trajList:
+            lat, long = zip(*traj)
+            # adding the lines joining the nodes
+            if i == 0:
+                fig = OsmGraph.__initGoFigure(lat, long, type='lines', label='path'+str(i))
+                
+            else:
+                fig = OsmGraph.__addTrace(fig, lat, long, type='lines', label='path'+str(i))
+            i += 1
+        i = 0
+        for label in pointDict:
+            lat, long = zip(*pointDict[label])
+            fig = OsmGraph.__addTrace(fig, lat, long, type='markers', label= label, size=15, color=colorList[i])
+            i += 1
+        # getting center for plots:
+        lat_center = np.mean(lat)
+        long_center = np.mean(long)
+        OsmGraph.__plotFigAndSave(fig, lat_center, long_center, filename)
 
 
     
@@ -153,5 +181,6 @@ class GraphFromGdfs(OsmGraph):
         
 class GraphFromPlace(OsmGraph):
     def __init__(self, placeQuery):
+        # e.g.: GraphFromPlace("Minnesota, USA")
         self.graph = ox.graph_from_place(placeQuery, network_type='drive')
         self.nodesGdf, self.edgesGdf = self.graphToGdfs()
